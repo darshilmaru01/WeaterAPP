cd45835a63832188f2ef182f3bfb65eed71f563d
Darshil Maru <darshilmaru4@gmail.com>
2022-06-11 10:51:10 +0530

    commit h

--- weather/views.py
+++ weather/views.py
@@ -4,8 +4,10 @@ from weather.forms import CityForm
 from weather.models import City
 
 # Create your views here.
+
+
 def index(request):
-    url= 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=18b3cc6df90337a70f2ed42c0ba62937'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=18b3cc6df90337a70f2ed42c0ba62937'
     city = 'Las Vegas'
 
     if request.method == 'POST':
@@ -20,15 +22,15 @@ def index(request):
 
     for city in cities:
         r = requests.get(url.format(city)).json()
-    
-        city_weather={
-            'city' : city,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
+
+        city_weather = {
+            'city': city,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
         }
 
         weather_data.append(city_weather)
-        
-    context = {'weather_data' : weather_data, 'form' : form}
+
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
